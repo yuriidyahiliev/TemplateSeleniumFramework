3b567a6e827e428371c6d9b04b23420cd1846850
ydi <ydi@ciklum.com>
2019-03-26 09:18:04 +0200

    added browsers implementation

--- src/main/java/core/browsers/Chrome.java
+++ src/main/java/core/browsers/Chrome.java
@@ -1,4 +1,28 @@
 package core.browsers;
 
-public class Chrome  {
+import io.github.sskorol.config.XmlConfig;
+import io.github.sskorol.core.Browser;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Chrome implements Browser {
+    @Override
+    public Name name() {
+        return Name.Chrome;
+    }
+
+    @Override
+    public Capabilities configuration(XmlConfig config) {
+        final ChromeOptions options = new ChromeOptions();
+        options.setCapability("enableVNC", true);
+        options.setCapability("enableVideo", true);
+        options.setCapability("name", config.getTestName());
+        options.setCapability("screenResolution", "1280x1024x24");
+        return merge(config, options);
+    }
+
+    @Override
+    public boolean isRemote() {
+        return true;
+    }
 }
--- src/main/java/core/browsers/Firefox.java
+++ src/main/java/core/browsers/Firefox.java
@@ -1,4 +1,27 @@
 package core.browsers;
 
-public class Firefox {
+import io.github.sskorol.config.XmlConfig;
+import io.github.sskorol.core.Browser;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class Firefox implements Browser {
+    @Override
+    public Name name() {
+        return Name.Firefox;
+    }
+
+    @Override
+    public Capabilities configuration(XmlConfig config) {
+        final FirefoxOptions options = new FirefoxOptions();
+        options.setCapability("enableVNC", true);
+        options.setCapability("name", config.getTestName());
+        options.setCapability("screenResolution", "1280x1024x24");
+        return merge(config, options);
+    }
+
+    @Override
+    public boolean isRemote() {
+        return true;
+    }
 }
